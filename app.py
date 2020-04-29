ef5557e12e50633c6635f94a3c6ac0677be3e04a
tuanthanhnguyen <63715128+tuanthanhnguyen@users.noreply.github.com>
2020-04-29 10:48:20 +0700

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from pydub import AudioSegment
-
-wav_audio = AudioSegment.from_file("audio.wav", format="wav")
-raw_audio = AudioSegment.from_file("audio.wav", format="raw",
-                                   frame_rate=44100, channels=2, sample_width=2)
-
-wav_audio.export("audio1.mp3", format="mp3")
-raw_audio.export("audio2.mp3", format="mp3")
+from flask import Flask, render_template 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/')
+def home():
+    return render_template("home.html")
